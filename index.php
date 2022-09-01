16ad91c2f3124b774032bd230683d3be423f8dfa
Marianela <84550042+MarianelaNovelli@users.noreply.github.com>
2022-09-01 16:51:57 -0300

    Modificaciones de diseño

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 $pagina = 'inicio';
 require_once('includes/config.php');
+require_once('includes/conexion.php');
+
+$stmt = $conexion->prepare("SELECT * FROM categorias");
+$stmt->execute();
+
+$categorias = $stmt->fetchAll();
+
 require_once('includes/header.php');
+
 ?>
 
   <section class="seccion-bienvenida">
     <div class="contenedor">
       <h1>SpeedService</h1>
-      <p><i>El sitio que te permite generar ganancias al volante y pedir un servicio ahora.</i></p>
+      <p><i>El sitio que te permite generar ganancias al volante y pedir un servicio ahora. </i></p>
       <a href="#">Nuestros Servicios</a>
     </div>
   </section>
@@ -18,38 +26,22 @@ require_once('includes/header.php');
       
       <div class="row">
 
-        <div class="col-md-4 mb-3">
-          <div class="card">
-            <img src="img/servicio_remis.jpg" class="card-img-top imagen-servicios" alt="remis">
-            <div class="card-body">
-              <h5 class="card-title">REMIS</h5>
-              <p class="card-text"><i>Si querés moverte, nosotros te llevamos.</i></p>
-              <a href="#" class="btn d-grid gap-2 col-10 mx-auto boton-servicio">Ir</a>
+      <?php
+        foreach($categorias as $fila){
+          echo '
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <img src="img/categorias/'.$fila['imgCategoria'].'" class="card-img-top imagen-servicios" alt="remis">
+                <div class="card-body">
+                  <h5 class="card-title">'.ucfirst($fila['categoria']).'</h5>
+                  <p class="card-text"><i>'.$fila['descripcionCategoria'].'</i></p>
+                  <a href="paginas/servicios.php?idCategoria='.$fila['idCategoria'].'" class="btn d-grid gap-2 col-10 mx-auto boton-servicio">Ir</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3">
-          <div class="card">
-            <img src="img/servicio_flete.jpg" class="card-img-top imagen-servicios" alt="flete">
-            <div class="card-body">
-              <h5 class="card-title">FLETE</h5>
-              <p class="card-text"><i>Dejá tu carga en buenas manos.</i></p>
-              <a href="#" class="btn d-grid gap-2 col-10 mx-auto boton-servicio">Ir</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3">
-          <div class="card">
-            <img src="img/servicio_mandado.jpg" class="card-img-top imagen-servicios" alt="mandado">
-            <div class="card-body">
-              <h5 class="card-title">MANDADO</h5>
-              <p class="card-text"><i>Ahorrá tiempo, nuestro destino es tu confianza.</i></p>
-              <a href="#" class="btn d-grid gap-2 col-10 mx-auto boton-servicio">Ir</a>
-            </div>
-          </div>
-        </div>
+          ';
+        }
+      ?>
 
       </div>
     </div>
